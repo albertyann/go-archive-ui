16f58426a609e8ef78d46b180172a3834ed407e9
albertyann <albertyann@gmail.com>
2024-10-02 00:00:23 +0800

    add land

---
+++ src/api/archive/land.js
@@ -0,0 +1,47 @@
+import request from '@/utils/request'
+
+// 查询TbLand列表
+export function listTbLand(query) {
+    return request({
+        url: '/api/v1/land',
+        method: 'get',
+        params: query
+    })
+}
+
+// 查询TbLand详细
+export function getTbLand (id) {
+    return request({
+        url: '/api/v1/land/' + id,
+        method: 'get'
+    })
+}
+
+
+// 新增TbLand
+export function addTbLand(data) {
+    return request({
+        url: '/api/v1/land',
+        method: 'post',
+        data: data
+    })
+}
+
+// 修改TbLand
+export function updateTbLand(data) {
+    return request({
+        url: '/api/v1/land/'+data.id,
+        method: 'put',
+        data: data
+    })
+}
+
+// 删除TbLand
+export function delTbLand(data) {
+    return request({
+        url: '/api/v1/land',
+        method: 'delete',
+        data: data
+    })
+}
+
