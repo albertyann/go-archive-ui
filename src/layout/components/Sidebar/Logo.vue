4f9eb7337091117a287888ab30f2952d2722977b
Albert Yann <albertyann@gmail.com>
2026-05-09 17:18:19 +0800

    fixed

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="sidebar-logo-container" :class="{'collapse':collapse}" :style="{ backgroundColor: $store.state.settings.themeStyle === 'light' ? (variables.menuLightBg || '#fff') : (variables.menuBg || '#001529') }">
+  <div class="sidebar-logo-container" :class="{'collapse':collapse}" :style="{ backgroundColor: $store.state.settings.themeStyle === 'light' ? '#fff' : '#001529' }">
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
         <img v-if="appInfo.sys_app_logo" :src="appInfo.sys_app_logo" class="sidebar-logo">
-        <h1 v-else class="sidebar-title" :style="{ color: $store.state.settings.themeStyle === 'dark' ? variables.sidebarTitle : variables.sidebarLightTitle }">{{ appInfo.sys_app_name }} </h1>
+        <h1 v-else class="sidebar-title" :style="{ color: $store.state.settings.themeStyle === 'dark' ? '#ffffff' : '#001529' }">{{ appInfo.sys_app_name }} </h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <img v-if="appInfo.sys_app_logo" :src="appInfo.sys_app_logo" class="sidebar-logo">
-        <h1 class="sidebar-title" :style="{ color: $store.state.settings.themeStyle === 'dark' ? variables.sidebarTitle : variables.sidebarLightTitle }">{{ appInfo.sys_app_name }} </h1>
+        <h1 class="sidebar-title" :style="{ color: $store.state.settings.themeStyle === 'dark' ? '#ffffff' : '#001529' }">{{ appInfo.sys_app_name }} </h1>
       </router-link>
     </transition>
   </div>
@@ -15,7 +15,6 @@
 
 <script>
 
-import variables from '@/styles/variables.scss'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -29,10 +28,7 @@ export default {
   computed: {
     ...mapGetters([
       'appInfo'
-    ]),
-    variables() {
-      return variables
-    }
+    ])
   }
 }
 </script>
